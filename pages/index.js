234621012a8cd1b03afd0f67d6f6913896fa9f59
Aaron Nguyen <me@aaronnguyen.dev>
2021-09-30 16:58:23 -0400

    Give grid items bigger breakpoints to fix footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import Container from "@mui/material/Container";
 import Copyright from "../src/Copyright";
 import Navbar from "../components/navbar/Navbar";
@@ -10,41 +10,31 @@ import ProjectList from "../components/project-list/ProjectList";
 
 export default function Index() {
   return (
-    /*  <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v5 example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-    
-*/
     <>
-      <Navbar />
-      <Container sx={{ mt: 2 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <About />
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Container sx={{ mt: 2 }}>
+          <Grid container spacing={2}>
+            <Grid item md={8}>
+              <About />
+            </Grid>
+            <Grid item md={4}>
+              <EventList />
+            </Grid>
+            <Grid item>
+              <Team />
+            </Grid>
+            <Grid item>
+              <ProjectList />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <EventList />
-          </Grid>
-          <Grid item xs={12}>
-            <Team />
-          </Grid>
-          <Grid item xs={12}>
-            <ProjectList />
-          </Grid>
-          <Grid item xs={12}>
-            <Copyright />
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </main>
+      <footer>
+        <Copyright />
+      </footer>
     </>
   );
 }
--- src/Copyright.js
+++ src/Copyright.js
@@ -1,16 +1,18 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
+import { Typography, Box } from "@mui/material";
 import MuiLink from "@mui/material/Link";
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Built with React.js, MUI, and Next.js by "}
-      <MuiLink color="inherit" href="https://www.aaronnguyen.dev">
-        Aaron Thang Nguyen
-      </MuiLink>
-      {" in "}
-      {new Date().getFullYear()}.
-    </Typography>
+    <Box>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {"Built with React.js, MUI, and Next.js by "}
+        <MuiLink color="inherit" href="https://www.aaronnguyen.dev">
+          Aaron Thang Nguyen
+        </MuiLink>
+        {" in "}
+        {new Date().getFullYear()}.
+      </Typography>
+    </Box>
   );
 }
