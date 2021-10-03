34b5bf9eb3e4391ad99217a7f944109d20855a6d
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-03 10:01:57 -0400

    Fix typography

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
               flexGrow={1}
               sx={{ textTransform: "uppercase" }}
             >
-              SSC @ VW
+              SCC @ VW
             </Typography>
 
             {/* Links */}
