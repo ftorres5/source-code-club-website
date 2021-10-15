685dc254b0ebd94d5db1fec5dbcbe52765ed5b97
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-14 22:35:31 -0400

    Add projects data

---
+++ components/data/projects-data.js
@@ -0,0 +1,20 @@
+const projectsData = [
+  {
+    id: 0,
+    name: "Official Website",
+    url: "https://github.com/aaronthangnguyen/source-code-club-website",
+    description:
+      "Official website of Source Code IT & Programming Club @ Valencia West",
+    category: ["Web Dev"],
+  },
+  {
+    id: 1,
+    name: "Check-in App",
+    url: "/",
+    description:
+      "To check-in students attending scheduled appointments with tutors, collect anonymous data for research purposes",
+    category: ["Web Dev"],
+  },
+];
+
+export default projectsData;
