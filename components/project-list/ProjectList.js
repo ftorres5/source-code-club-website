c3f4a5dcd9ec089bf45586b6bb210ff8148277fb
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-14 22:36:11 -0400

    Add Project List and Project Card

--- components/project-list/ProjectList.js
+++ components/project-list/ProjectList.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { Box } from "@mui/system";
 import { FolderOpen as FolderOpenIcon } from "@mui/icons-material";
+import { Stack } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState, useEffect } from "react";
+import projectsData from "../data/projects-data";
 import IconTypography from "../shared/IconTypography";
+import Project from "./Project";
 
 const ProjectList = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const data = projectsData;
+    setProjects(data);
+  }, []);
   return (
     <Box>
       <IconTypography icon={<FolderOpenIcon />}>Projects</IconTypography>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        spacing={1.5}
+        justifyContent="flex-start"
+      >
+        {projects.map((project) => (
+          <Project key={project.id} data={project} />
+        ))}
+      </Stack>
     </Box>
   );
 };
